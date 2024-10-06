2d66e91c80dc525402e1efb4e4f20104303c6250
dksah1 <dkboss127126@gmail.com>
2024-10-06 21:40:58 +0545

    added profile page

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,20 @@ import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
-
+// private route
+import PrivateRoute from "./components/PrivateRoute.jsx";
 // auth
 import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Register.jsx";
+import Profile from "./pages/User/Profile.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Route>
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Route>
