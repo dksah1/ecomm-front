67c10ce22dedc996d27ceeed6c7d2be13d507d8b
dksah1 <dkboss127126@gmail.com>
2024-10-15 23:17:28 +0545

    added product list page

---
+++ src/pages/Admin/ProductList.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  useCreateProductMutation,
+  useUploadProductImageMutation,
+} from "../../redux/api/productApiSlice";
+import { useFetchCategoriesQueries } from "../../redux/api/categoryApiSlice";
+import { toast } from "react-toastify";
+
+const ProductList = () => {
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCatgory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [brand, setBrand] = useState("");
+  const [stock, setStock] = useState(0);
+  const [imageUrl, setImageUrl] = useState(null);
+  const navigate = useNavigate();
+
+  return <div>ProductList</div>;
+};
+
+export default ProductList;
